cb56993c4adc0ff383cbac35521449f18e6a36d2
SerjNF <foreman@inbox.ru>
2018-10-31 23:21:24 +0700

    first

---
+++ src/main/java/ru/inbox/foreman/phonebook/controller/myController.java
@@ -0,0 +1,16 @@
+package ru.inbox.foreman.phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class myController {
+
+    @GetMapping("/get")
+    public String get(@RequestParam(name = "name", required = true, defaultValue = "World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
